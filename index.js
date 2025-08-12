d59f29e26ced9fe45a2f6365f976b92c78e01fa5
Thais Moura <thais.rock@live.com>
2025-08-11 23:40:47 -0300

    Desafio 1 DIO

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (xp < 1000) {
 } else if (xp >= 1001 && xp <= 2000) {
     nivel = "Bronze";
 } else if (xp >= 2001 && xp <= 5000) {
-    nivel = "Prata Ouro";
+    nivel = "Prata";
 } else if (xp >= 5001 && xp <= 8000) {
     nivel = "Platina Diamante";
 } else if (xp >= 8001 && xp <= 9000) {
